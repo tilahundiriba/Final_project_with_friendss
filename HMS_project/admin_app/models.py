eebbd091152f1728ad924f58afeef3346f66fddf
muluken getaye <getayemuluken9@gmail.com>
2024-02-17 14:40:19 +0300

    patient registration

--- HMS_project/admin_app/models.py
+++ HMS_project/admin_app/models.py
@@ -56,3 +56,36 @@ class UserProfile(AbstractUser):
     )
 def __str__(self):
     return f"{self.reset_key}"
+
+
+
+from django.db import models
+
+class Patient(models.Model):
+    patient_id = models.CharField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    birth_date = models.DateField()
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.patient_id
+    
+    
+    #patient history table
+
+class PatientHistory(models.Model):
+    PatientID=models.ForeignKey(Patient, on_delete=models.CASCADE)
+    Date=models.DateField()
+    Sympthom=models.CharField(max_length=100)
+    DiseaseName=models.CharField(max_length=100)
+    Nurse_ID=models.ForeignKey(UserAccount1,on_delete=models.CASCADE,related_name='doctor_history_set',related_query_name='doctor_history')
+    Doctor_ID=models.ForeignKey(UserAccount1,on_delete=models.CASCADE,related_name='nurse_history_set',related_query_name='doctor_history')
