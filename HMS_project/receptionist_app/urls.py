550bf1b30294f3cc862e9f372ddf12ea6ba089eb
muluken getaye <getayemuluken9@gmail.com>
2024-02-27 00:09:17 +0300

    editANDdelete

--- HMS_project/receptionist_app/urls.py
+++ HMS_project/receptionist_app/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
   path('receptionist_dash/', views.receptionist_dash, name='receptionist_dash'),
   path('dash_content/', views.receptionist_dash_content, name='receptionist_dash_content'),
   path('add-patient/', views.add_patient, name='add_patient'),
-  path('edit-patient/', views.edit_patient, name='edit-patient'),
+  path('edit-patient/<str:patient_id>/', views.edit_patient, name='edit-patient'),
   path('about_patient/', views.about_patient, name='about_patient'),
   path('dis_patient/', views.dis_patient, name='dis_patient'),
   path('dis_form/', views.dis_forms, name='dis_form'),
-  path('patient/<str:patient_id>/delete/', views.delete_patient, name='delete_patient'),
+  path('delete_patient/', views.delete_patient, name='delete_patient'),
+  path('update_patient/<str:patient_id>/', views.update_patient, name='update_patient'),
 
 
   #path('form/', views.form, name='form'),
--- HMS_project/receptionist_app/views.py
+++ HMS_project/receptionist_app/views.py
@@ -1,9 +1,10 @@
 
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.crypto import get_random_string
 from .models import PatientRegister
 from admin_app.models import User
+from django.db.models import Q
 
 def receptionist_dash(request):
     return render(request,'receptionist_dash/receptionist_dash.html')
@@ -47,23 +48,41 @@ def add_patient(request):
                 city=city,
                 region=region,
                 kebele=kebele,
-                staff=doctor_id,)
+                staff=doctor_id,
+                )
                   
         patient.save()   
         register=True 
     return render(request, 'receptionist_dash/add-patient.html',{'register':register})  # Render the form template initially
-from django.http import JsonResponse
 
-def delete_patient(request, patient_id):
-    if request.method == 'DELETE':
-        try:
-            patient = PatientRegister.objects.get(patient_id=patient_id)
-            patient.delete()
-            return JsonResponse({'message': 'Patient deleted successfully'})
-        except PatientRegister.DoesNotExist:
-            return JsonResponse({'message': 'Patient not found'}, status=404)
-    
-    return render(request,'receptionist_dash/patients.html')
+def delete_patient(request):
+    if request.method == 'POST':
+        search_query = request.POST.get('search_query')
+        patients = PatientRegister.objects.filter(
+             Q(patient_id__icontains=search_query) |
+             Q(first_name__icontains=search_query) |
+             Q(middle_name__icontains=search_query) |
+             Q(last_name__icontains=search_query) |
+             Q(gender__icontains=search_query) |
+             Q(birth_date__icontains=search_query) |
+             Q(age__icontains=search_query) |
+             Q(phone_number__icontains=search_query) |
+             Q(email__icontains=search_query) |
+             Q(country__icontains=search_query) |
+             Q(city__icontains=search_query) |
+             Q(region__icontains=search_query) |
+             Q(kebele__icontains=search_query) |
+             Q(staff__icontains=search_query)
+)
+        if 'delete' in request.POST:
+            # Handle deletion of selected employees
+            patient_delete = request.POST.getlist('patient_delete')
+            PatientRegister.objects.filter(patient_id__in=patient_delete).delete()
+            
+    else:
+        patients = PatientRegister.objects.all()
+
+    return render(request, 'receptionist_dash/patients.html', {'patients': patients})
 def dis_patient(request):
     patients = PatientRegister.objects.all()
     return render(request,'receptionist_dash/patients.html', {'patients':patients})
@@ -73,7 +92,105 @@ def receptionist_dash(request):
     return render(request,'receptionist_dash/receptionist_dash.html')
 def receptionist_dash_content(request):
     return render(request,'receptionist_dash/dash_content.html')
-def edit_patient(request):
-    return render(request,'receptionist_dash/edit-patient.html')
+# def edit_patient(request):
+    # if 'update' in request.POST:
+   # Handle deletion of selected employees
+            #  patient_delete = request.POST.getlist('patient_delete')
+            #  try:
+                #  patient = PatientRegister.objects.filter(patient_id__in=patient_delete)
+                #  first_name = request.POST.get('first_name')
+                #  middle_name = request.POST.get('middle_name')
+                #  last_name = request.POST.get('last_name')
+                #  gender = request.POST.get('gender')
+                #  birth_date = request.POST.get('birth_date')
+                #  age = request.POST.get('age')
+                #  phone_number = request.POST.get('phone_number')
+                #  email = request.POST.get('email')
+                #  country = request.POST.get('country')
+                #  city = request.POST.get('city')
+                #  region = request.POST.get('region')
+                #  kebele = request.POST.get('kebele')
+                #  doctor_id = request.POST.get('doctor_id')
+                #  patient = PatientRegister.objects.create(
+                            # first_name=first_name,
+                            # middle_name=middle_name,
+                            # last_name=last_name,
+                            # gender=gender,
+                            # birth_date=birth_date,
+                            # age=age,
+                            # phone_number=phone_number,
+                            # email=email,
+                            # country=country,
+                            # city=city,
+                            # region=region,
+                            # kebele=kebele,
+                            # staff=doctor_id,)
+                            #   
+                #  patient.save()   
+# 
+                #  return redirect('dis_patient')
+            #  except PatientRegister.DoesNotExist:
+                #  pass
+    # 
+    # return redirect('dis_patient')
+def edit_patient(request,patient_id):
+     try:
+       patientid = get_object_or_404(PatientRegister, pk=patient_id)
+ #   std = Student.objects.get(pk=roll)
+       return render(request,'receptionist_dash/edit-patient.html',{'patientid':patientid})
+     except PatientRegister.DoesNotExist:
+       return render(request,'receptionist_dash/edit-patient.html',{'message':'patient  not found'})
+     
+ 
+    # return render(request,'receptionist_dash/edit-patient.html')
 def dis_forms(request):
     return render(request,'receptionist_dash/form.html')
+def update_patient(request,patient_id):
+   first_name = request.POST.get('first_name')
+   middle_name = request.POST.get('middle_name')
+   last_name = request.POST.get('last_name')
+   age = request.POST.get('age')
+   phone_number = request.POST.get('phone_number')
+   email = request.POST.get('email')
+   country = request.POST.get('country')
+   city = request.POST.get('city')
+   region = request.POST.get('region')
+   kebele = request.POST.get('kebele')
+   doctor_id = request.POST.get('doctor_id')
+   
+   patient = PatientRegister.objects.get(pk=patient_id)
+   patient.first_name=first_name
+   patient.middle_name=middle_name
+   patient.last_name=last_name
+   patient.age=age
+   patient.phone_number=phone_number
+   patient.email=email
+   patient.country=country
+   patient.city=city
+   patient.region=region
+   patient.kebele=kebele
+   patient.staff=doctor_id
+   patient.save()
+   return redirect('dis_patient')
+# def update(request,roll):
+    # try:
+        # std = get_object_or_404(Student, pk=roll)
+      #std = Student.objects.get(pk=roll)
+        # return render(request,'secondapp/update.html',{'std':std})
+    # except Student.DoesNotExist:
+        #   return render(request,'secondapp/update.html',{'message':'Student not found'})
+# def do_update(request ,roll):
+    # std_roll = request.POST.get('roll')
+    # std_name = request.POST.get('name')
+    # std_email = request.POST.get('email')
+    # std_address = request.POST.get('address')
+    # std_phone = request.POST.get('phone')
+# 
+    # std = Student.objects.get(pk=roll)
+    # std.roll = std_roll
+    # std.name = std_name
+    # std.email = std_email
+    # std.address = std_address
+    # std.phone = std_phone
+    # std.save()
+    # return redirect('home')
