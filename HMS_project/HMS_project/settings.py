1deef178f25abd0e5b2fb0a0d35978fe8e8b025c
muluken getaye <getayemuluken9@gmail.com>
2024-03-09 16:08:17 +0300

    for model issue

--- HMS_project/HMS_project/settings.py
+++ HMS_project/HMS_project/settings.py
@@ -145,24 +145,24 @@ WSGI_APPLICATION = 'HMS_project.wsgi.application'
 #         'PORT':'5432',
 #     }
 # }
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',  # Adjust the path to your SQLite database file
-   }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE':'django.db.backends.postgresql',
-#         'NAME':'postgres',
-#         'USER':'postgres',
-#         'PASSWORD':'362588',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
+#    'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',  # Adjust the path to your SQLite database file
+#    }
 # }
-
+# 
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'nf',
+        'USER':'postgres',
+        'PASSWORD':'362588',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
+# 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
